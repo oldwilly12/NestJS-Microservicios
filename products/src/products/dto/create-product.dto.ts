e48239a7616aa92d28a5f7f385b48a8fc347940a
Fracisco Acosta <rwilly203@gmail.com>
2025-03-07 16:55:08 -0600

    starting CRUD
    Starting CRUD

--- products/src/products/dto/create-product.dto.ts
+++ products/src/products/dto/create-product.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProductDto {}
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsNumber()
+    @Type(() => Number) // trasnfromar a numero el string cuando no llega de JSON
+    price: number;
+
+
+}
